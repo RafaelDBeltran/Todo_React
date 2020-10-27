62d038467f575985c741095c2c6293493f44b514
RDBeltran <rafaelbeltran.aluno@unipampa.edu.br>
2020-10-27 10:13:56 -0300

    Menu, Home, about

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import PageHeader from '../template/pagHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'/>
+        <h2>Nossa História</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
     </div>
 )
